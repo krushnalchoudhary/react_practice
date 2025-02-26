a16c0ff15a9fdc3ca95b153c1921b5982bedc5b8
krushnalchoudhary <krishnalchoudhary9827@gmail.com>
2025-02-26 13:07:50 +0530

    26 feb

---
+++ reduxtoolkit_todo/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>Hello</h1>
+    <AddTodo />
+    <Todos />
+    </>
+  )
+}
+
+export default App
